2b53a10fd4ead06677cae305a584e2fb0b26110f
rajat1433 <rajat.mttl@gmail.com>
2018-09-06 23:33:14 +0530

    Hii

--- app/modules/dashboard/controllers/recent.js
+++ app/modules/dashboard/controllers/recent.js
@@ -15,7 +15,7 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
           description: "A news Recommender system was built to retrieve news articles and present them to users based on the degree of relevancy to their search query. News Articles were clustered based on their tf-idf vectors, Wordnet sysnsets were used to increase the coverage of potentially relevant news articles. A user query was served with summarized news from all sources using Natural language processing techniques. Experimental results showed that the proposed approach performed well and retrieved meaningful results. ",
           image: "reco",
           theme: "warning",
-          url:"fp360.us:830",
+          url:"#",
           
       },
       {
@@ -25,7 +25,7 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
           description: "Using RNN,LSTM,CNN and ensemble models made a model to detect the duplicate questions in websites like Quora and Stack overflow. Used Natural Language Processing Libraries in the models. Implemented the Glove model and various current research papers in this field and try to optimize the accuracy and F1 score by searching for hyper parameters and feature engineering.",
           image: "ques",
           theme:"info",
-          url: "fp360.us:459",
+          url: "#",
          
           
       },
@@ -36,7 +36,7 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
           description: "Angular Bootstrap Dashboard is a web dashboard application based on Bootstrap and AngularJS.",
           image: "dashboard",
           theme: "success",
-          url: "dashboard.ranjithprabhu.in",
+          url: "#",
           
       },
       {
@@ -46,7 +46,7 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
           description: "Made a Health Care Portal to enable users to maintain a complete record of their medical history. Also includes an Online Appointment system , each user has a unique login id, symptom tests , blogs etc. All their information was linked to their Aadhar Card number. The Records of each patient was stored in a Database using MongoDB. Technologies used were HTML, CSS, Angular, Javascript, PHP, MongoDB and WAMP Server.",
           image: "health",
           theme: "danger",
-          url: "fp360.us:202",
+          url: "#",
          
       },
       {
@@ -56,7 +56,7 @@ function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
           description: "Made an Android Application to measure the number of the lens required to correct the myopia. Provide Eye exercises ,tests and blindness checker.",
           image: "fundo",
           theme: "primary",
-          url: "fp360.us:7077",
+          url: "#",
           
       }
     ];
