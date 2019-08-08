bed6590db6fab8db66ec2545eb916bd2cf928507
rajat1433 <rajat.mttl@gmail.com>
2019-08-08 18:45:42 +0530

    test9

--- app/modules/dashboard/controllers/gsoc.js
+++ app/modules/dashboard/controllers/gsoc.js
@@ -6,7 +6,7 @@
 
  dashboard.controller("GSoCController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash',
  function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
- 
+    console.log("coming to GSoC controller");
  }]);
  
  
