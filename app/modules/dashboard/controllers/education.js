94faf52d05fc52332b29e8535c1510ace2cb10a2
rajat1433 <rajat.mttl@gmail.com>
2019-08-17 23:31:46 +0530

    changes

--- app/modules/dashboard/controllers/education.js
+++ app/modules/dashboard/controllers/education.js
@@ -1,11 +1,8 @@
 ﻿/*==========================================================
 
-
-
  ===========================================================*/
 
 dashboard.controller("EducationController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash',
 function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
 
 }]);
-
